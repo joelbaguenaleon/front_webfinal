67e420e510ea42c2b4c546a11e14d591cc215707
joelbaguenaleon <joelbagueenaleon@gmail.com>
2026-07-20 22:35:14 +0200

    Corregir carga HTTPS del CSS

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,53 +1,65 @@
-<title>NBAguena</title>
-<h1>NBAguena</h1>
-<link rel="stylesheet" href="{{ asset('style.css') }}">
+<!DOCTYPE html>
+<html lang="es">
 
-<div class="menu">
-    <a href="/"><button>Home</button></a>
-    <a href="/regular-season"><button>Estadídticas jugadores temporada regular 2026</button></a>
-    <a href="/playoffs"><button>Estadídticas jugadores playoffs 2026</button></a>
-    <a href="/chatbot"><button>ChatBotNBA</button></a>
-</div>
-<h2>Una web diseñada para consultar información actual de la NBA.</h2>
-<h3>🚨La temporada 2025/26 ha terminado. Debido a que la liga se encuentra en período de traspasos, la web podría
-    presentar fallos en las plantillas. Les pedimos disculpas, ya estamos trabajando en ello.🚨</h3>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NBAguena</title>
+    <link rel="stylesheet" href="/style.css?v=30">
+</head>
+
+<body>
+    <h1>NBAguena</h1>
+    <div class="menu">
+        <a href="/"><button>Home</button></a>
+        <a href="/regular-season"><button>Estadídticas jugadores temporada regular 2026</button></a>
+        <a href="/playoffs"><button>Estadídticas jugadores playoffs 2026</button></a>
+        <a href="/chatbot"><button>ChatBotNBA</button></a>
+    </div>
+    <h2>Una web diseñada para consultar información actual de la NBA.</h2>
+    <h3>🚨La temporada 2025/26 ha terminado. Debido a que la liga se encuentra en período de traspasos, la web podría
+        presentar fallos en las plantillas. Les pedimos disculpas, ya estamos trabajando en ello.🚨</h3>
 
 
-<div class="home-grid">
-    <section class="home-card">
-        <h2>Partidos NBA</h2>
-        <h4>Se pueden consultar todos los partidos de la NBA.</h4>
+    <div class="home-grid">
+        <section class="home-card">
+            <h2>Partidos NBA</h2>
+            <h4>Se pueden consultar todos los partidos de la NBA.</h4>
 
-        <form method="GET" action="/">
-            <input type="date" name="fecha" value="{{ $fecha }}">
-            <button type="submit">Buscar</button>
-        </form>
+            <form method="GET" action="/">
+                <input type="date" name="fecha" value="{{ $fecha }}">
+                <button type="submit">Buscar</button>
+            </form>
 
-        @if (count($partidos))
-            @foreach ($partidos as $partido)
-                <span class="game-type">
-                    {{ $partido['postseason'] ? '🏆 Playoffs Game' : '🏀 Regular Season Game' }}
-                </span>
+            @if (count($partidos))
+                @foreach ($partidos as $partido)
+                    <span class="game-type">
+                        {{ $partido['postseason'] ? '🏆 Playoffs Game' : '🏀 Regular Season Game' }}
+                    </span>
 
-                <div class="game-card">
-                    <strong>{{ $partido['visitor_team']['full_name'] }}</strong>
-                    {{ $partido['visitor_team_score'] }}
-                    -
-                    {{ $partido['home_team_score'] }}
-                    <strong>{{ $partido['home_team']['full_name'] }}</strong>
+                    <div class="game-card">
+                        <strong>{{ $partido['visitor_team']['full_name'] }}</strong>
+                        {{ $partido['visitor_team_score'] }}
+                        -
+                        {{ $partido['home_team_score'] }}
+                        <strong>{{ $partido['home_team']['full_name'] }}</strong>
 
-                    <p>{{ $partido['status'] }}</p>
-                </div>
-            @endforeach
-        @else
-            <p>No hay partidos para esa fecha.</p>
-        @endif
-    </section>
-    <section class="home-card">
-        <h2>🏆 Bracket Final Playoffs 2026</h2>
+                        <p>{{ $partido['status'] }}</p>
+                    </div>
+                @endforeach
+            @else
+                <p>No hay partidos para esa fecha.</p>
+            @endif
+        </section>
+        <section class="home-card">
+            <h2>🏆 Bracket Final Playoffs 2026</h2>
 
-        <a href="{{ asset('bracket.png') }}" target="_blank">
-            <img src="{{ asset('bracket.png') }}" class="bracket-img" alt="Bracket Playoffs">
-        </a>
-    </section>
-</div>
+            <a href="{{ asset('bracket.png') }}" target="_blank">
+                <img src="{{ asset('bracket.png') }}" class="bracket-img" alt="Bracket Playoffs">
+            </a>
+        </section>
+    </div>
+
+</body>
+
+</html>
